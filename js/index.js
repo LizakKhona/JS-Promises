7aa3e7a5e529564672246c2df3794fdda1084e44
LizakKhona <liz.honahbeeva2009@gmail.com>
2026-09-20 17:22:27 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,43 +1,43 @@
 // 1
 
 
-// const delay = ms => {
-//     return new Promise((resolve) => {
-//       setTimeout(() => {
-//           resolve(ms)
-//       }, ms)
-//   })
-// };
+const delay = ms => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+          resolve(ms)
+      }, ms)
+  })
+};
 
-// const logger1 = (time) => console.log(`Resolved after ${time}ms`);
+const logger1 = (time) => console.log(`Resolved after ${time}ms`);
 
 
-// delay(2000).then(logger1); 
+delay(2000).then(logger1); 
 
 
 // 2
 
-// const users = [
-//   { name: "Mango", active: true },
-//   { name: "Poly", active: false },
-//   { name: "Ajax", active: true },
-//   { name: "Lux", active: false },
-// ];
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
 
-// const toggleUserState = (allUsers, userName, callback) => {
-//     return new Promise((resolve) => {
-//         const updatedUsers = allUsers.map((user) =>
-//           user.name === userName ? { ...user, active: !user.active } : user,
-//         );
-//         resolve(updatedUsers)
-//     })
+const toggleUserState = (allUsers, userName, callback) => {
+    return new Promise((resolve) => {
+        const updatedUsers = allUsers.map((user) =>
+          user.name === userName ? { ...user, active: !user.active } : user,
+        );
+        resolve(updatedUsers)
+    })
   
-// };
+};
 
-// const logger2 = updatedUsers => console.table(updatedUsers);
+const logger2 = updatedUsers => console.table(updatedUsers);
 
-// toggleUserState(users, 'Mango').then(logger2);
-// toggleUserState(users, 'Lux').then(logger2);
+toggleUserState(users, 'Mango').then(logger2);
+toggleUserState(users, 'Lux').then(logger2);
 
 
 
